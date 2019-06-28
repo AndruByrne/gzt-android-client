a920b3cd8c8b605bde689164ea03564a69f4bbe5
Andrew Byrne <byrnean@vmware.com>
2019-06-28 01:46:46 -0700

    Added Renderer client(draw) and controller(input) listener to SettingsActivity

--- app/src/main/java/com/anthropicandroid/gzt/activity/GZTSettingsActivity.java
+++ app/src/main/java/com/anthropicandroid/gzt/activity/GZTSettingsActivity.java
@@ -1,27 +1,46 @@
 package com.anthropicandroid.gzt.activity;
 
 import android.databinding.DataBindingUtil;
+import android.opengl.Matrix;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.anthropicandroid.gzt.R;
 import com.anthropicandroid.gzt.ZombieTrackerApplication;
 import com.anthropicandroid.gzt.databinding.GztSettingsActivityBinding;
 import com.anthropicandroid.gzt.modules.SansUserSettingsAdapterComponent;
+import com.google.vr.ndk.base.DaydreamApi;
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
+import com.google.vr.sdk.controller.Controller;
+import com.google.vr.sdk.controller.ControllerManager;
 
 import javax.inject.Inject;
+import javax.microedition.khronos.egl.EGLConfig;
 
 import rx.functions.Action1;
 
-final public class GZTSettingsActivity extends AppCompatActivity {
-
+public class GZTSettingsActivity extends AppCompatActivity {
     public static final String TAG = GZTSettingsActivity.class.getSimpleName();
+    private static final int EXIT_FROM_VR_REQUEST_CODE = 42;
 
     @Inject public MapViewLifecycleHolder mapViewHolder;
     @Inject public BottomNavControllers bottomNavControllers;
+
+    private GZTSettingsView settingsView;
+    private Renderer renderer;
+    private ControllerManager controllerManager;
+    private Controller controller;
+    // Given an intent with a media file and format, this will load the file and generate the mesh.
+    private MediaLoader mediaLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +94,24 @@ final public class GZTSettingsActivity extends AppCompatActivity {
 //                ));
     }
 
+    /**
+     * Tries to exit gracefully from VR using a VR transition dialog.
+     *
+     * @return whether the exit request has started or whether the request failed due to the device
+     * not being Daydream Ready
+     */
+    private boolean exitFromVr() {
+        // This needs to use GVR's exit transition to avoid disorienting the user.
+        DaydreamApi api = DaydreamApi.create(this);
+        if (api != null) {
+            api.exitFromVr(this, EXIT_FROM_VR_REQUEST_CODE, null);
+            // Eventually, the Activity's onActivityResult will be called.
+            api.close();
+            return true;
+        }
+        return false;
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -115,5 +152,109 @@ final public class GZTSettingsActivity extends AppCompatActivity {
 
     private void backpressNotHandled() {
         super.onBackPressed();
+    }
+
+    /**
+     * Standard GVR renderer. Most of the real work is done by {@link SceneRenderer}.
+     */
+    private class Renderer implements GvrView.StereoRenderer {
+        private static final float Z_NEAR = .1f;
+        private static final float Z_FAR = 100;
+
+        // Used by ControllerEventListener to manipulate the scene.
+        public final SceneRenderer scene;
+
+        private final float[] viewProjectionMatrix = new float[16];
+
+        /**
+         * Creates the Renderer and configures the VR exit button.
+         *
+         * @param parent Any View that is already attached to the Window. The uiView will secretly be
+         *               attached to this View in order to properly handle UI events.
+         */
+        @MainThread
+        public Renderer(ViewGroup parent) {
+            Pair<SceneRenderer, GZTSettingsView> pair
+                    = SceneRenderer.createForVR(GZTSettingsActivity.this, parent);
+            scene = pair.first;
+            settingsView = pair.second;
+
+            settingsView.setVrIconClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (!exitFromVr()) {
+                                // Directly exit Cardboard Activities.
+                                onActivityResult(EXIT_FROM_VR_REQUEST_CODE, RESULT_OK, null);
+                            }
+                        }
+                    });
+        }
+
+        @Override
+        public void onNewFrame(HeadTransform headTransform) {
+        }
+
+        @Override
+        public void onDrawEye(Eye eye) {
+            Matrix.multiplyMM(
+                    viewProjectionMatrix, 0, eye.getPerspective(Z_NEAR, Z_FAR), 0, eye.getEyeView(), 0);
+            scene.glDrawFrame(viewProjectionMatrix, eye.getType());
+        }
+
+        @Override
+        public void onFinishFrame(Viewport viewport) {
+        }
+
+        @Override
+        public void onSurfaceCreated(EGLConfig config) {
+            scene.glInit();
+            mediaLoader.onGlSceneReady(scene);
+        }
+
+        @Override
+        public void onSurfaceChanged(int width, int height) {
+        }
+
+        @Override
+        public void onRendererShutdown() {
+            scene.glShutdown();
+        }
+    }
+
+    /**
+     * Forwards Controller events to SceneRenderer.
+     */
+    private class ControllerEventListener extends Controller.EventListener
+            implements ControllerManager.EventListener {
+        private boolean touchpadDown = false;
+        private boolean appButtonDown = false;
+
+        @Override
+        public void onApiStatusChanged(int status) {
+            Log.i(TAG, ".onApiStatusChanged " + status);
+        }
+
+        @Override
+        public void onRecentered() {
+        }
+
+        @Override
+        public void onUpdate() {
+            controller.update();
+
+            renderer.scene.setControllerOrientation(controller.orientation);
+
+            if (!touchpadDown && (controller.clickButtonState || controller.triggerButtonState)) {
+                renderer.scene.handleClick();
+            }
+
+            if (!appButtonDown && controller.appButtonState) {
+                renderer.scene.toggleUi();
+            }
+
+            touchpadDown = controller.clickButtonState || controller.triggerButtonState;
+            appButtonDown = controller.appButtonState;
+        }
     }
 }
